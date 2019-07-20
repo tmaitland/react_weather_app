12648d6f766b1be8453bacc9efd6a33e92da7d59
tmaitland <tmaitland@techlaunch.io>
2019-07-19 22:52:55 -0400

    Formatting and layout of data looks decent

--- src/components/CityWeather.js
+++ src/components/CityWeather.js
@@ -7,48 +7,58 @@ const capitalize = {
     textTransform: 'capitalize'
 }
 
+const tableStyles = {
+	width: '80%',
+    textAlign: 'left',
+    /* display: flex; */
+    /* flex-direction: column; */
+    /* justify-content: center; */
+    padding: '2rem 2rem'
+}
+
+const weatherInfo = {
+	display: 'flex',
+	justifyContent: 'center',
+}
+
 const CityWeather = props => (
-	<div className="weather__info">
-		<table>
+	<div className="weather__info" style={weatherInfo}>
+		<table style={tableStyles}>
 			{
 				props.icon && <tr className="weather__key"><td> Icon:</td>
 					<td><img src={`${iconurl}${ props.icon }${imgext}`} alt="weather icon" className="weather_icon" /></td>
 				</tr>
 			}
 			{	
-				props.city && props.country && <p className="weather__key"> Location: 
-					<span className="weather__value"> { props.city }, { props.country }</span>
-				</p> 
+				props.city && props.country && <tr className="weather__key"><td>Location:</td> 
+					<td className="weather__value"> { props.city }, { props.country }</td>
+				</tr> 
 			}
-				<td>Temperature</td>
-				<td>Humidity</td>
-				<td>Conditions</td>
+			{ 	
+				props.temperature && <tr className="weather__key"><td>Temperature: </td> 
+					<td className="weather__value"> { Math.round(props.temperature) }&#176;F</td>
+				</tr> 
+			}
+			{ 	
+				props.humidity && <tr className="weather__key"><td>Humidity: </td> 
+					<td className="weather__value"> { props.humidity }% </td>
+				</tr> 
+			}
+			{ 	
+				props.description && <tr className="weather__key"><td> Conditions: </td>
+					<td className="weather__value" style={capitalize} > { props.description } </td>
+				</tr> 
+			}
 		
 		</table>
-     {/* {
-         props.icon && <p className="weather__key"> Icon:
-            <img src={`${iconurl}${ props.icon }${imgext}`} alt="weather icon" className="weather_icon" />
-         </p>
-     } */}
-	
-	 { 	
-	 	props.temperature && <p className="weather__key"> Temperature: 
-	 		<span className="weather__value"> { Math.round(props.temperature) }&#176;F</span>
-	 	</p> 
-	 }
-	 { 	
-	 	props.humidity && <p className="weather__key"> Humidity: 
-	 		<span className="weather__value"> { props.humidity }% </span>
-	 	</p> 
-	 }
-	 { 	
-	 	props.description && <p className="weather__key"> Conditions: 
-	 		<span className="weather__value" style={capitalize}> { props.description } </span>
-	 </p> 
-	 }
+   
 	 { 
 	 	props.error && <p className="weather__error">{ props.error }</p>  
 	 }
+	
+	
+	 
+	 
 	</div>
 );
 
